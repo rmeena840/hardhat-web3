b1fd747a9d6eb4c367dad2fed66e0902d7ead238
Ravinda <ravindra.iitism@gmail.com>
2023-08-24 20:44:25 +0530

    add your own task in hardhat

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,9 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require('./tasks/block-number')
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: "0.8.21",
 };
 
 export default config;
